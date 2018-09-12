7d8301e4ceb2cbb881d35aa94eb843525c91437a
baldrani <mael.mayon@free.fr>
2018-09-12 19:27:38 +0200

    :rocket:

---
+++ docker/django/django.values.js
@@ -0,0 +1,7 @@
+'use strict';
+
+module.exports = {
+    first_project_name: 'MyProject',
+    port: '8000',
+    container_name: 'django_ju7ydhcjlrykgwj',
+};
---
+++ docker/postgres/postgres.values.js
@@ -0,0 +1,10 @@
+'use strict';
+
+module.exports = {
+    database_name: 'skyflow',
+    user: 'skyflow',
+    password: 'root',
+    database_storage_location: '../.database/my_app',
+    port: '5432',
+    container_name: 'postgres_ju7ydhcjlrykgwk',
+};
