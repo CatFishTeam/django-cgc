88f88ac20896648073e9339a9a9778b464b32d4c
guillaumepn <guillaumepn@free.fr>
2018-10-07 20:33:55 +0200

    add django-crispy-forms install through Docker + finished user registration + basic models + forms integration for register / login

--- hearthstone/models.py
+++ hearthstone/models.py
@@ -1,3 +1,48 @@
 from django.db import models
+from django.contrib.auth.models import User
+from django.utils import timezone
+from django.db.models.signals import post_save
+from django.dispatch import receiver
 
-# Create your models here.
+
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    credit = models.IntegerField(default=200)
+
+
+@receiver(post_save, sender=User)
+def create_user_profile(sender, instance, created, **kwargs):
+    if created:
+        Profile.objects.create(user=instance)
+
+
+@receiver(post_save, sender=User)
+def save_user_profile(sender, instance, **kwargs):
+    instance.profile.save()
+
+
+class Deck(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    title = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.title
+
+
+class Card(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    deck = models.ForeignKey(Deck, on_delete=models.SET_NULL, null=True)
+    title = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.title
+
+
+class Game(models.Model):
+    user1 = models.ForeignKey(User, on_delete=models.CASCADE, related_name='player_one')
+    user2 = models.ForeignKey(User, on_delete=models.CASCADE, related_name='player_two')
+    winner = models.ForeignKey(User, on_delete=models.CASCADE)
+    date = models.DateTimeField(default=timezone.now)
+
+    def __str__(self):
+        return self.winner
--- hearthstone/views.py
+++ hearthstone/views.py
@@ -1,23 +1,39 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from django.template import loader
+from .forms import UserRegisterForm
+from django.contrib import messages
+
+from .models import Card, Deck, Game
+
 
 # Create your views here.
 
 def home(request):
     title = 'Accueil'
-    test = 'wow'
     context = {
         'title': title,
-        'test': test,
+        'games': Game.objects.all()
     }
     return render(request, 'hearthstone/index.html', context)
 
+
 def register(request):
-    return render(request, 'registration/register.html')
+    if request.method == 'POST':
+        form = UserRegisterForm(request.POST)
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data.get('username')
+            messages.success(request, f'Le compte de {username} a bien été créé !')
+            return redirect('home')
+    else:
+        form = UserRegisterForm()
+    return render(request, 'registration/register.html', {'form': form})
+
 
 def game(request):
     return render(request, 'hearthstone/game.html')
+
 
 def test(request):
     test = 'hehe'
