7a6f829f2edd8fa3fe86773cb6e9e3e3da4f3531
Baldrani <mael.mayon@free.fr>
2018-11-09 14:32:42 +0100

    Test Creation Deck

--- hearthstone/static/hearthstone/main.js
+++ hearthstone/static/hearthstone/main.js
@@ -1,4 +1,18 @@
-$('#createDeck .single-card').on('click', () => {
-    this.find('.cardNumber')
-    console.log(this)
-})
+$('#createDeck .single-card').click( function() {
+    //TODO
+    if($(this).find('.single-card__number').text() > 0) {
+        $(this).find('.single-card__number').text($(this).find('.single-card__number').text() - 1);
+    }
+
+    $card = $(this)
+
+    $('.deck').append("<div>" + $card.data('name') + "</div>");
+    $('.deck div').each( function() {
+        if($(this).text() == $card.data('name')) {
+            console.log('Coucou');
+        } else {
+            $('.deck').append("<div>" + $card.data('name') + "</div>");
+        }
+    });
+    //console.log($(e.target).find('.single-card__number').html())
+});
