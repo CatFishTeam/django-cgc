be2cf39c174552dd67db771810bb5f5f421d358d
Baldrani <mael.mayon@free.fr>
2018-11-16 00:54:16 +0100

    Add a lot of thinkgs

--- assets/scripts/pages/createDeck.js
+++ assets/scripts/pages/createDeck.js
@@ -1,7 +1,7 @@
 import Cookies from 'js-cookie'
 import * as Toastr from "toastr";
 
-let deck = []; //Tuple {"id": cardId, "count": number}
+let deck = []; //{"id": cardId, "count": number}
 
 $('#createDeck .single-card').click( function() {
     decreaseCard($(this));
@@ -9,36 +9,64 @@ $('#createDeck .single-card').click( function() {
 
 function addToDeck(card) {
     const cardId = card.data('id');
-    if(deck.length > 0) {
-        deck.forEach( (e) => {
-            if(e['id'] === cardId){
-                e['count'] += 1;
-                throw BreakException;
-            } else {
-                deck.push({'id': cardId, 'count': 1})
-            }
-        })
-    } else {
-        deck.push({'id': cardId, 'count': 1})
-    }
-    console.log(deck)
+    const cardName = card.data('name');
+    let exit = false;
+    deck.forEach( (e) => { //use some()
+        if(e['id'] === cardId){
+            e['count'] += 1;
+            exit = true;
+        }
+    });
+    if(exit) return;
+    deck.push({'id': cardId, 'count': 1, 'name': cardName});
 }
 
 function decreaseCard(card) {
     let quantity = card.find('.single-card__number').text();
     if( quantity > 0) {
-        quantity -= 1
+        quantity -= 1;
         card.find('.single-card__number').text( quantity );
         if (quantity == 0){
             card.css('opacity', 0.4)
         }
         addToDeck(card)
+        displayDeck()
     }
+}
+
+function displayDeck() {
+    $('.deck__container').empty();
+    deck.forEach((e)=> {
+        console.log(e)
+        $('.deck__container').append('<div class="deck__card" data-id="' + e.id + '">' + e.name + '<span>x' + e.count + '</span></div>')
+    })
+}
+
+$('body').on('click', '.deck__card', function() {
+    removeFromDeck($(this).data('id'))
+});
+
+function removeFromDeck(cardId){
+    let exit = false;
+    deck.forEach( (e) => {
+        if(e['id'] === cardId){
+            if(e['count'] > 1) {
+                e['count'] -= 1;
+            } else {
+                var filteredItems = this.items.filter(function (e) {
+                    return e !== item;
+                });
+            }
+        }
+    });
+    if(exit) return;
+    deck.push({'id': cardId, 'count': 1, 'name': cardName});
+    console.log(deck)
 }
 
 let link2Deck
 $('#save-deck').click( () => {
-    postData(`/save-deck`, {title: $('#deck-title').val(), deck: deck})
+    postData(`/save-deck`, {title: $('#deck__title').val(), deck: deck})
         .then(data => link2Deck = '/show-deck/' + JSON.stringify(data))
         .catch(error => console.error(error))
         .then(() => {
