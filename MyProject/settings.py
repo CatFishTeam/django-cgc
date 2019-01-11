2bc1008ca61b79240f7c7e131191ee3e9f089396
guillaumepn <guillaumepn@free.fr>
2019-01-11 16:51:46 +0100

    test heroku deploy

--- MyProject/settings.py
+++ MyProject/settings.py
@@ -128,12 +128,12 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
-STATIC_ROOT = os.path.join(BASE_DIR, 'assets')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
 
 # Extra places for collectstatic to find static files.
 STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'assets'),
+    os.path.join(BASE_DIR, 'staticfiles'),
 )
 
 LOGIN_REDIRECT_URL = '/'
