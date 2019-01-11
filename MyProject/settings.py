635f901ef3d18541eb0d46957ee9a49f28318241
guillaumepn <guillaumepn@free.fr>
2019-01-11 17:01:56 +0100

    test heroku deploy + j'en ai OFFICIELLEMENT plein le cul

--- MyProject/settings.py
+++ MyProject/settings.py
@@ -133,7 +133,7 @@ STATIC_URL = '/static/'
 
 # Extra places for collectstatic to find static files.
 STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'staticfiles'),
+        os.path.join(BASE_DIR, 'assets'),
 )
 
 LOGIN_REDIRECT_URL = '/'
